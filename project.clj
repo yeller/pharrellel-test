1ec59b628776d6bfbe3b24c5e7581e8e43ca6b4c
Tom Crayford <tcrayford@googlemail.com>
2014-03-25 12:52:22 +0000

    fix up project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject pharrellel-test "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A pharrellel test runner for clojure"
+  :url "https://github.com/tcrayford/pharrellel-test"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
